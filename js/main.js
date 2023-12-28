d73f4d259a5e0208d33a699fd569ae9bbec91b44
Davi Duarte <davi.d.santos8@aluno.senai.br>
2023-12-28 19:59:58 -0300

    quintas finais

--- js/main.js
+++ js/main.js
@@ -1,29 +1,70 @@
-import { obterInformacoesPersonagem } from './service.js';
+const characterId = document.getElementById('characterId');
+const btnGo = document.getElementById('btn-go');
+const btnReset = document.getElementById('btn-reset');
+const content = document.getElementById('content');
+const conteinerResult = document.getElementById('result-style');
+const image = document.getElementById('img');
 
+const fetchApi = (value) => {
+  const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    return data;
+  });
 
-function exibirInformacoesPersonagem(character) {
-  const characterInfoElement = document.getElementById('character-info');
-
-
-  const html = ` 
-    <h2> <p> Nome: ${character.name}</h2>
-    <img src="${character.image}" alt="${character.name}">
-    <p>Status: ${character.status}</p>
-    <p>Espécie: ${character.species}</p>
-    <p>Gênero: ${character.gender}</p>
-    <p>Planeta de Origem: ${character.origin.name}</p>
-  `;
-
-  characterInfoElement.innerHTML = html;
+  return result;
 }
 
+const keys = ['name', 'status', 'species', 'gender', 'origin', ];
+const newKeys = {
+  name: 'Nome',
+  status: 'Status',
+  species: 'Espécie',
+  gender: 'Gênero',
+  origin: 'Planeta de origem',
+  
+}
 
+const buildResult = (result) => {
+  return keys.map((key) => document.getElementById(key))
+    .map((elem) => {
+      if(elem.checked === true && (Array.isArray(result[elem.name])) === true){
+        const arrayResult = result[elem.name].join('\r\n');
+        console.log(arrayResult);
+        const newElem = document.createElement('p');
+        newElem.innerHTML = `${newKeys[elem.name]}: ${arrayResult}`;
+        content.appendChild(newElem);
+      } else if(elem.checked === true && (elem.name === 'origin')){
+        const newElem = document.createElement('p');
+        newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name].name}`;
+        content.appendChild(newElem);
+      } else if(elem.checked === true && typeof(result[elem.name]) !== 'object'){
+        const newElem = document.createElement('p');
+        newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name]}`;
+        content.appendChild(newElem);
+      }
+    });
+}
 
+btnGo.addEventListener('click', async (event) => {
+  event.preventDefault();
 
-obterInformacoesPersonagem(118)
-  .then(data => {
-    exibirInformacoesPersonagem(data);
-  })
-  .catch(error => {
-    console.error('Erro ao obter informações do personagem:', error.message);
-  });
+  if(characterId.value === ''){
+    return content.innerHTML = 'É necessário fazer um filtro.';
+  }
+
+  const result = await fetchApi(characterId.value);
+  if(content.firstChild === null){
+    conteinerResult.className = 'result-style';
+    image.src = `${result.image}`;
+    buildResult(result);
+  } else {
+    content.innerHTML = '';
+    conteinerResult.className = 'result-style';
+    image.src = `${result.image}`;
+    buildResult(result);
+  }
+});
+
+btnReset.addEventListener('click', () => location.reload());
